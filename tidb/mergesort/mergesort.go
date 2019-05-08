d5ab77a5fcb804690c3741e28ed13b1bf8ebc424
__hidehalo <tianchen_cc@yeah.net>
2019-05-08 18:44:31 +0800

    update

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	// "fmt"
+)
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 
@@ -8,7 +11,7 @@ func MergeSort(src []int64) {
 	len := len(src)
 
 	if len > 1 {
-		if len <= 2048 { 
+		if len <= 2048 {
 			// Sequential
 			mergesort(src)
 		} else {
@@ -26,22 +29,52 @@ func MergeSort(src []int64) {
 			MergeSort(src[middle:])
 
 			wg.Wait()
-			merge(src[:middle], src[middle:])
+			copyi64arr(src, merge(src[:middle], src[middle:]))
 		}
 	}
 }
 
-func merge(left []int64, right []int64) {
-	
-
+func merge(left []int64, right []int64)  []int64 {
+	i,j := 0,0
+	sLeft,sRight := len(left),len(right)
+	outPlace := make([]int64, 0, sLeft+sRight)
+	for i < sLeft && j < sRight {
+		if left[i] < right[j] {
+			outPlace = append(outPlace, left[i])
+			i++
+		} else {
+			outPlace = append(outPlace, right[j])
+			j++
+		}
+	}
+	for i < sLeft {
+		outPlace = append(outPlace, left[i])
+		i++
+	}
+	for j < sRight {
+		outPlace = append(outPlace, right[j])
+		j++
+	}
+	return outPlace
 }
-
 
 func mergesort(src []int64) {
 	if len(src) > 1 {
 		middle := len(src) / 2
 		mergesort(src[:middle])
 		mergesort(src[middle:])
-		merge(src[:middle], src[middle:])
+		copyi64arr(src, merge(src[:middle], src[middle:]))
 	}
 }
+
+func copyi64arr(a []int64, b []int64) {
+	for i,v := range b {
+		a[i] = v
+	}
+}
+
+// func main() {
+// 	src := []int64{11,10,9,8,7,6,5,4,3,2,1}
+// 	MergeSort(src)
+// 	fmt.Println(src)
+// }
