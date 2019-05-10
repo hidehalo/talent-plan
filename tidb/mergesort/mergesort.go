d4142ece97b87878f561b419e0279485b9363d8a
__hidehalo <tianchen_cc@yeah.net>
2019-05-10 12:06:10 +0800

    update

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -7,35 +7,43 @@ import (
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"runtime/trace"
 	"sync"
 	"time"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+var cpuprofile = flag.String("cpu", "", "write cpu profile to `file`")
+var memprofile = flag.String("mem", "", "write memory profile to `file`")
+var tracefile = flag.String("trace", "", "write trace to `file`")
+
+var threshold = 1 << 11
+var poolSize = 1
 
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
 	len := len(src)
 	if len > 1 {
-		if len <= 2048 {
+		if len <= threshold {
 			// Sequential
-			mergesort(src)
+			mergesortNormal(src)
 		} else {
 			// Parallel
 			middle := len / 2
-
 			var wg sync.WaitGroup
-			wg.Add(1)
-
+			wg.Add(poolSize)
 			go func() {
 				defer wg.Done()
 				MergeSort(src[:middle])
 			}()
-
-			MergeSort(src[middle:])
-
+			if poolSize == 2 {
+				go func() {
+					defer wg.Done()
+					MergeSort(src[middle:])
+				}()
+			} else {
+				MergeSort(src[middle:])
+			}
 			wg.Wait()
 			copy(src, merge(src[:middle], src[middle:]))
 		}
@@ -44,9 +52,8 @@ func MergeSort(src []int64) {
 
 func merge(left []int64, right []int64) []int64 {
 	i, j := 0, 0
-	sLeft, sRight := len(left), len(right)
-	outPlace := make([]int64, 0, sLeft+sRight)
-	for i < sLeft && j < sRight {
+	outPlace := make([]int64, 0, len(left)+len(right))
+	for i < len(left) && j < len(right) {
 		if left[i] < right[j] {
 			outPlace = append(outPlace, left[i])
 			i++
@@ -55,22 +62,22 @@ func merge(left []int64, right []int64) []int64 {
 			j++
 		}
 	}
-	for i < sLeft {
+	for i < len(left) {
 		outPlace = append(outPlace, left[i])
 		i++
 	}
-	for j < sRight {
+	for j < len(right) {
 		outPlace = append(outPlace, right[j])
 		j++
 	}
 	return outPlace
 }
 
-func mergesort(src []int64) {
+func mergesortNormal(src []int64) {
 	if len(src) > 1 {
 		middle := len(src) / 2
-		mergesort(src[:middle])
-		mergesort(src[middle:])
+		mergesortNormal(src[:middle])
+		mergesortNormal(src[middle:])
 		copy(src, merge(src[:middle], src[middle:]))
 	}
 }
@@ -84,6 +91,7 @@ func internalPrepare(src []int64) {
 
 func main() {
 	flag.Parse()
+
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
@@ -95,19 +103,33 @@ func main() {
 		}
 		defer pprof.StopCPUProfile()
 	}
-	// ... rest of the program ...
 	if *memprofile != "" {
 		f, err := os.Create(*memprofile)
 		if err != nil {
 			log.Fatal("could not create memory profile: ", err)
 		}
 		defer f.Close()
-		runtime.GC() // get up-to-date statistics
+		runtime.GC()
 		if err := pprof.WriteHeapProfile(f); err != nil {
 			log.Fatal("could not write memory profile: ", err)
 		}
 	}
+	if *tracefile != "" {
+		f, err := os.Create(*tracefile)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		err = trace.Start(f)
+		if err != nil {
+			panic(err)
+		}
+		defer trace.Stop()
+	}
+
 	src := make([]int64, 1<<20)
 	internalPrepare(src)
+
 	MergeSort(src)
 }
