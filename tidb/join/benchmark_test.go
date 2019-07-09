aa5c86a667655271f1aaab409304c63dac85a8cb
__hidehalo <tianchen_cc@yeah.net>
2019-07-09 18:18:48 +0800

    并没有什么软用的更新

--- tidb/join/benchmark_test.go
+++ tidb/join/benchmark_test.go
@@ -2,22 +2,27 @@ package main
 
 import "testing"
 
-func BenchmarkJoin(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Join("./t/r0.tbl", "./t/r2.tbl", []int{0}, []int{1})
-	}
-}
+// func BenchmarkJoin(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		Join("./t/r0.tbl", "./t/r2.tbl", []int{0}, []int{1})
+// 	}
+// }
 
-func BenchmarkJoinExample(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		JoinExample("./t/r0.tbl", "./t/r2.tbl", []int{0}, []int{1})
-	}
-}
+// func BenchmarkJoinExample(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		JoinExample("./t/r0.tbl", "./t/r2.tbl", []int{0}, []int{1})
+// 	}
+// }
 
 func BenchmarkReadCSV(b *testing.B) {
-	// TODO:impl
+	for i := 0; i < b.N; i++ {
+		readCSVFileIntoTbl("./t/r0.tbl")
+	}
 }
 
 func BenchmarkParalleReadCSV(b *testing.B) {
-	// TODO:impl
+	for i := 0; i < b.N; i++ {
+		p := NewParalleCSVReader(10)
+		p.ReadCSV("./t/r0.tbl")
+	}
 }
--- tidb/join/join.go
+++ tidb/join/join.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"sync"
+)
+
 // Join accepts a join query of two relations, and returns the sum of
 // relation0.col0 in the final result.
 // Input arguments:
@@ -10,37 +18,62 @@ package main
 // Output arguments:
 //   sum: sum of relation0.col0 in the final result
 func Join(f0, f1 string, offset0, offset1 []int) (sum uint64) {
-	pcr := new(ParalleCSVReader)
-	panic(pcr)
 	return sum
 }
 
 type ParalleCSVReader struct {
-	readChann []byte	// Read file channel,line by line
-	contents [][]byte	// Contents of file,split via EOL
-	concurrency uint	// Concurrency
-	scaner bufio.Scanner
-	wg sync.WaitGroup
+	readChan    []chan []byte // Read file channel,line by line
+	contents    [][][]byte    // Contents of file,split via EOL
+	concurrency uint          // Concurrency
 }
 
-func (pcr *ParalleCSVReader) readCSV(filepath string) {
-	for i:=0;i<pcr.concurrency;i++ {
-		pcr.wg.Add(1)
-		go func () {
-			defer pcr.wg.Done()
-			pcr.contents = append(pcr.contents, <-pcr.readChann)
+func NewParalleCSVReader(concurrency uint) *ParalleCSVReader {
+	pcr := &ParalleCSVReader{
+		readChan:    make([]chan []byte, concurrency),
+		contents:    make([][][]byte, 0),
+		concurrency: concurrency,
+	}
+	for i := uint(0); i < concurrency; i++ {
+		pcr.readChan[i] = make(chan []byte, 500)
+	}
+
+	return pcr
+}
+
+func (pcr *ParalleCSVReader) ReadCSV(filepath string) {
+	csvFile, _ := os.Open(filepath)
+	reader := bufio.NewReader(csvFile)
+	var wg sync.WaitGroup
+
+	for i := uint(0); i < pcr.concurrency; i++ {
+		go func(id uint) {
+			for record := range pcr.readChan[id] {
+				recordBytes := bytes.Split([]byte{','}, record)
+				pcr.contents = append(pcr.contents, recordBytes)
+				wg.Done()
+			}
+		}(i)
+	}
+
+	c := uint(0)
+	for {
+		c %= pcr.concurrency
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
 		}
+		wg.Add(1)
+		pcr.readChan[c] <- line
+		c++
 	}
-	for pcr.scaner.Scan() {
-		pcr.readChann <- pcr.scaner.Bytes()
-	}
-	pcr.wg.Wait();
+
+	wg.Wait()
 }
 
-func buildHashTable(data [][]string, offset []int) (hashtable *mvmap.MVMap) {
+// func buildHashTable(data [][]string, offset []int) (hashtable *mvmap.MVMap) {
 
-}
+// }
 
-func probe(hashtable *mvmap.MVMap, row []string, offset []int) (rowIDs []int64) {
+// func probe(hashtable *mvmap.MVMap, row []string, offset []int) (rowIDs []int64) {
 
-}
+// }
